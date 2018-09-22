dbe09fad23605cb25c18368a066404f63143dc15
Felipe Ordonez <carlosfelipe29@gmail.com>
2018-09-21 21:33:49 -0500

    uso de promesas en codijo JS para consultas

--- clase7/reto/funciones.js
+++ clase7/reto/funciones.js
@@ -40,14 +40,14 @@ var avatars=[
     'https://vignette.wikia.nocookie.net/legomessageboards/images/1/1d/Chewbacca.jpg/revision/latest?cb=20130430154940'
 ];
 
-
+//-- USO DE PROMESA  ---
 function api_request(url){
     return new Promise( function(resolve, reject){
         var httpP = new XMLHttpRequest();
         httpP.onreadystatechange = function(){
             if (this.readyState==4){
                 if(this.status==200) {
-                    resolve(this.responseText);
+                    resolve(JSON.parse(this.responseText));
                 }else {
                     reject(Error(' '+this.status));
                 }
@@ -59,16 +59,6 @@ function api_request(url){
         }
         httpP.send();
     });
-}
-
-function listar_info(informacion){
-    var tabla = document.getElementById('lista_repos');
-    var auxT = "";
-    informacion.forEach(function(nombres,index){
-            auxT += "<tr><th scope='row'>"+ nombres.name + "</th><td><a href='"+nombres.html_url+"' target='_blank'>link "+(index+1)+"</a></td><td>"                
-    })
-    document.getElementById("tabla_repos").setAttribute("style","display: block;");
-    tabla.innerHTML = auxT;
 }
 
 ///--------------------    RETO   -----------------------------------//
@@ -101,7 +91,7 @@ function selector(){
     seleccion= opc;
     var aux = "<option value=0>Seleccionar</option>";
     api_request(url).then(function(data){
-        data_api=JSON.parse(data);
+        data_api=data;
         var titulo_select= document.getElementById("title_select"); // toma el object titulo del select
         var opc_sel = document.getElementById("opciones");
         var opc1 = menu_op(opc);
@@ -115,12 +105,15 @@ function selector(){
             else    
                 aux += "<option value='" + (index+1) +"'>"+item.name +"</option>"
         });
-    opc_sel.innerHTML = aux;
+        opc_sel.innerHTML = aux;
     })
     .catch(function(err){
         console.log(err);
+        document.getElementById("div_img").setAttribute("style","display: block;");
+        document.getElementById("div_img").innerHTML = "<h3><strong>Error en consulta. Vuelva a Intentarlo</strong></h3>";
     }); 
 }
+
 
 var select_button =  document.getElementById("opciones");   // var que recibe Objeto Select
 
@@ -130,56 +123,119 @@ select_button.onchange = function() { //detecta cambios sobre el select cargado
         var datos = [];
         var url_consulta = "";
         url_consulta= urls_selector[indice-1];  // tiene la lista o URL del elemento seleccionado 
-        consultas(url_consulta, indice);
-}
-
-// FUNCION DONDE HARE UNA PROMESA  o un callback   y depende del boton  ejecute los url respectivos
-function consultas(url_consulta, indice){
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            data = JSON.parse(this.responseText);  
-            datos = data;
-            despliegue_info(datos, indice);
+        if(seleccion == "people"){
+            api_request(url_consulta).then(function(data){
+                personaje(data, indice);
+            })
+            .catch(function(err){
+                console.log("Error");
+                document.getElementById("div_img").setAttribute("style","display: block;");
+                document.getElementById("div_img").innerHTML = "<h2><strong>Error en consulta. Vuelva a Intentarlo</strong></h2>";
+            });
         }
-    }
-    req.open("GET", url_consulta, true);
-    req.send();
+        else if(seleccion == "films"){
+            api_request(url_consulta).then(function(data){
+                films(data, indice);
+            })
+            .catch(function(err){
+                console.log("Error");
+                document.getElementById("div_img").setAttribute("style","display: block;");
+                document.getElementById("div_img").innerHTML = "<h2><strong>Error en consulta. Vuelva a Intentarlo</strong></h2>";
+            });
+
+        }
+        else if(seleccion == "planets"){
+            api_request(url_consulta).then(function(data){
+                planets(data, indice);
+            })
+            .catch(function(err){
+                console.log("Error");
+                document.getElementById("div_img").setAttribute("style","display: block;");
+                document.getElementById("div_img").innerHTML = "<h2><strong>Error en consulta. Vuelva a Intentarlo</strong></h2>";
+            });
+            
+
+        }
+        else if(seleccion == "species"){
+            api_request(url_consulta).then(function(data){
+                species(data, indice);
+            })
+            .catch(function(err){
+                console.log("Error");
+                document.getElementById("div_img").setAttribute("style","display: block;");
+                document.getElementById("div_img").innerHTML = "<h2><strong>Error en consulta. Vuelva a Intentarlo</strong></h2>";
+            });
+            
+        }
+        else if(seleccion == "starships"){
+            api_request(url_consulta).then(function(data){
+                starships(data, indice);
+            })
+            .catch(function(err){
+                console.log("Error");
+                document.getElementById("div_img").setAttribute("style","display: block;");
+                document.getElementById("div_img").innerHTML = "<h2><strong>Error en consulta. Vuelva a Intentarlo</strong></h2>";
+            }); 
+        }
+        else if(seleccion == "vehicles"){
+            api_request(url_consulta).then(function(data){
+                vehicles(data, indice);
+            })
+            .catch(function(err){
+                console.log("Error");
+                document.getElementById("div_img").setAttribute("style","display: block;");
+                document.getElementById("div_img").innerHTML = "<h2><strong>Error en consulta. Vuelva a Intentarlo</strong></h2>";
+            }); 
+        }
 }
 
 function despliegue_info(datos, indice){
-    if(seleccion == "people"){
-        document.getElementById("div_vehiculo").value="";
-        document.getElementById("div_naves").value ="";
-        document.getElementById("data_films").value ="";
-        document.getElementById("div_img").setAttribute("style","display: block;");
-        document.getElementById("foto").setAttribute("src",avatars[indice-1]);
-        document.getElementById("info_people_container").setAttribute("style","display: block;");
-        document.getElementById("info_people").value ="Nacimiento: "+ datos.birth_year+"\nGenero: "+datos.gender+"\naltura: "+datos.height+"\nMasa: "+ datos.mass+"\n";
-        document.getElementById("data_people_container").setAttribute("style","display: block;");
-        aux_vehiculo = "";
-        aux_naves = "";
-        aux_films = "";
-        //document.getElementById("data_people").value =datos.homeworld+"\nNaves: "+ datos.starships+"\n";
-        get_planet(datos.homeworld); // funcion para data planeta
-        var arr_nave = datos.starships;
-        var arr_vehiculo = datos.vehicles;
-        var arr_films = datos.films;
-        size_ships= arr_nave.length;
-        size_vehicles= arr_vehiculo.length;
-        size_films= arr_films.length;
-        arr_nave.forEach(function(nave,index){
-            get_naves(nave); // funcion para naves
-        });
-        arr_vehiculo.forEach(function(medio,index){
-            get_vehiculo(medio); // funcion para naves
-        });
-        arr_films.forEach(function(movie,index){
-            get_films(movie); // funcion para naves
-        });
-    }
-    if(seleccion == "films"){
-        aux_films_char="";
+}
+
+//--     nueva funcion para ser ejecutada dentro de Promesa de Personajes  --//
+function personaje(datos, indice){
+    document.getElementById("div_vehiculo").value="";
+    document.getElementById("div_naves").value ="";
+    document.getElementById("data_films").value ="";
+    document.getElementById("div_img").setAttribute("style","display: block;");
+    document.getElementById("foto").setAttribute("src",avatars[indice-1]);
+    document.getElementById("info_people_container").setAttribute("style","display: block;");
+    document.getElementById("info_people").value ="Nacimiento: "+ datos.birth_year+"\nGenero: "+datos.gender+"\naltura: "+datos.height+"\nMasa: "+ datos.mass+"\n";
+    document.getElementById("data_people_container").setAttribute("style","display: block;");
+    aux_vehiculo = "";
+    aux_naves = "";
+    aux_films = "";
+    get_planet(datos.homeworld); // funcion para data planeta
+    var arr_nave = datos.starships;
+    var arr_vehiculo = datos.vehicles;
+    var arr_films = datos.films;
+    size_ships= arr_nave.length;
+    size_vehicles= arr_vehiculo.length;
+    size_films= arr_films.length;
+    arr_nave.forEach(function(nave,index){
+        get_naves(nave); // funcion para naves
+    });
+    arr_vehiculo.forEach(function(medio,index){
+        get_vehiculo(medio); // funcion para naves
+    });
+    // USO DE PROMISES.ALL  para ejecutar las URL de las Movies del Personaje consultado
+    var p_movies = [];
+    arr_films.forEach(function(url){
+        ctd_films+=1;
+        p_movies.push(api_request(url));
+    });
+    Promise.all(p_movies).then(function(data){
+        var hsAux = "";
+        data.forEach(function(peli,index){
+            hsAux+="- "+peli.title+"\n";
+        })
+        document.getElementById("data_films").value =hsAux;
+        ctd_films=0;
+    });
+}
+
+function films(datos, indice){
+    aux_films_char="";
         aux_films_planet="";
         aux_films_ships = "";
         aux_films_species = "";
@@ -205,58 +261,63 @@ function despliegue_info(datos, indice){
         arr_species.forEach(function(esp,index){
             get_films_species(esp,index); // funcion para naves
         });
-    }
-    if(seleccion == "planets"){
-        aux_planets_people="";
-        aux_planets_films="";
-        aux_planets_info = "";
-        var arr_resident = datos.residents;        
-        var arr_pelis = datos.films;
-        document.getElementById("info_planet_container").setAttribute("style","display: block;");
-        document.getElementById("data_planet_container").setAttribute("style","display: block;");
-        get_planet_info(datos);
-        arr_pelis.forEach(function(peli,index){
-            get_planet_films(peli,index); // funcion para naves
-        });
-        arr_resident.forEach(function(res,index){
-            get_films_resid(res,index); // funcion para naves
-        });   
-    }
-    if(seleccion == "species"){
-        aux_species_films="";
-        aux_species_info="";
-        var arr_peli = datos.films;        
-        document.getElementById("info_species_container").setAttribute("style","display: block;");
-        document.getElementById("data_species_container").setAttribute("style","display: block;");
-        get_species_info(datos);
-        arr_peli.forEach(function(fil,index){
-            get_species_films(fil,index); // funcion para pelis donde aparecen
-        });
-        get_species_planet(data.homeworld); // el planeta de la especie
-    }
-    if(seleccion == "starships"){
-        aux_ships_films="";
-        aux_ships_info="";
-        var ship_films = datos.films;        
-        document.getElementById("info_ships_container").setAttribute("style","display: block;");
-        document.getElementById("data_ships_container").setAttribute("style","display: block;");
-        get_ships_info(datos);
-        ship_films.forEach(function(fil,index){
-            get_ships_films(fil); // funcion para pelis donde aparecen
-        });
-    }
-    if(seleccion == "vehicles"){
-        aux_auto_films="";
-        aux_auto_info="";
-        var auto_films = datos.films;        
-        document.getElementById("info_auto_container").setAttribute("style","display: block;");
-        document.getElementById("data_auto_container").setAttribute("style","display: block;");
-        get_auto_info(datos);
-        auto_films.forEach(function(fil,index){
-            get_auto_films(fil); // funcion para pelis donde aparecen
-        });
-    }
 }
+
+
+function planets(datos, indice){
+    aux_planets_people="";
+    aux_planets_films="";
+    aux_planets_info = "";
+    var arr_resident = datos.residents;        
+    var arr_pelis = datos.films;
+    document.getElementById("info_planet_container").setAttribute("style","display: block;");
+    document.getElementById("data_planet_container").setAttribute("style","display: block;");
+    get_planet_info(datos);
+    arr_pelis.forEach(function(peli,index){
+        get_planet_films(peli,index); // funcion para naves
+    });
+    arr_resident.forEach(function(res,index){
+        get_films_resid(res,index); // funcion para naves
+    });   
+}
+
+function species(datos, index){
+    aux_species_films="";
+    aux_species_info="";
+    var arr_peli = datos.films;        
+    document.getElementById("info_species_container").setAttribute("style","display: block;");
+    document.getElementById("data_species_container").setAttribute("style","display: block;");
+    get_species_info(datos);
+    arr_peli.forEach(function(fil,index){
+        get_species_films(fil,index); // funcion para pelis donde aparecen
+    });
+    get_species_planet(datos.homeworld); // el planeta de la especie
+}
+
+function starships(datos, index){
+    aux_ships_films="";
+    aux_ships_info="";
+    var ship_films = datos.films;        
+    document.getElementById("info_ships_container").setAttribute("style","display: block;");
+    document.getElementById("data_ships_container").setAttribute("style","display: block;");
+    get_ships_info(datos);
+    ship_films.forEach(function(fil,index){
+        get_ships_films(fil); // funcion para pelis donde aparecen
+    });
+}
+
+function vehicles(datos, index){
+    aux_auto_films="";
+    aux_auto_info="";
+    var auto_films = datos.films;        
+    document.getElementById("info_auto_container").setAttribute("style","display: block;");
+    document.getElementById("data_auto_container").setAttribute("style","display: block;");
+    get_auto_info(datos);
+    auto_films.forEach(function(fil,index){
+        get_auto_films(fil); // funcion para pelis donde aparecen
+    });
+}
+
 
 //para cargar informacion mas interna
 function get_planet(url){   
@@ -317,27 +378,6 @@ function get_naves(url){
     req.send();
 }
 
-
-function get_films(url){ 
-    var data = [];
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            data = JSON.parse(this.responseText);  
-            ctd_films+=1;
-            var hsAux="- "+data.title+"\n";
-            aux_films+=hsAux;
-            if(ctd_films == size_films){
-                //document.getElementById('div_naves').innerHTML = "<h3>Naves:</h3>"+aux_naves;
-                document.getElementById("data_films").value =aux_films;
-                ctd_films=0;
-            }
-        }
-    }
-    req.open("GET", url, true);
-    req.send();
-}
-
 function get_films_characters(url, indice){   // personajes de  peliculas
     var data = [];
     var req = new XMLHttpRequest();
@@ -349,13 +389,6 @@ function get_films_characters(url, indice){   // personajes de  peliculas
                 aux_films_char+=hsAux;    
                 document.getElementById("films_people").value =aux_films_char;
             }
-            /*
-            //var hsAux=`<div><ul><li>${data.name}</li></ul></div>`;
-            if(ctd_persona == ){
-                //document.getElementById('div_naves').innerHTML = "<h3>Naves:</h3>"+aux_naves;
-                document.getElementById("div_naves").value =aux_naves;
-                ctd_persona=0;
-            }*/
         }
     }
     req.open("GET", url, true);
@@ -373,13 +406,6 @@ function get_films_planets(url, indice){   // personajes de  peliculas
                 aux_films_planet+=hsAux;    
                 document.getElementById("films_planet").value =aux_films_planet;
             }
-            /*
-            //var hsAux=`<div><ul><li>${data.name}</li></ul></div>`;
-            if(ctd_persona == ){
-                //document.getElementById('div_naves').innerHTML = "<h3>Naves:</h3>"+aux_naves;
-                document.getElementById("div_naves").value =aux_naves;
-                ctd_persona=0;
-            }*/
         }
     }
     req.open("GET", url, true);
@@ -563,8 +589,6 @@ function get_auto_films(url){   // naves en peliculas
     req.send();
 }
 
-
-
 function menu_op(opc){
     var sel;
     switch(opc){
@@ -606,17 +630,3 @@ function clear_screen(){
     document.getElementById("info_auto_container").setAttribute("style","display: none;");
     document.getElementById("data_auto_container").setAttribute("style","display: none;");
 }
-
-
-/*
-imgFilms.forEach(function(item){
-    var hsAux=`<div class="col-md-4">
-                <img src="${item.url}" width="100%" alt="">
-                <button class="btn btn-success"> Abrir</button>
-            </div>`;
-    ux_naves += hsAux;
-   
-});
-document.getElementById('imagenes').innerHTML = htmlString;
-*/
-//para cargar informacion Naves
